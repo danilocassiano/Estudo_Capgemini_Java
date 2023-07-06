1d825b3762aabbbc314315d69e596a8c64fd3109
danilocassiano <danilocassiano@hotmail.com>
2023-07-05 23:25:44 -0300

    Atividade da Aula 1
    Aulas sobre tipos de Dados,

---
+++ MeuPrimeiroProj.java
@@ -0,0 +1,8 @@
+public class MeuPrimeiroProj{
+    public static void main(String[] args){
+       final float numero;
+        numero = 855.65f;
+        System.out.println("Valor do numero é: " + numero);
+
+    }
+}
