bb1a1a5f5ee3930b994e469cfc36cd286cda7ab5
danilocassiano <danilocassiano@hotmail.com>
2023-07-13 22:28:52 -0300

    update arquivos.

--- ComandosCondicionais/src/ComandoCondicional.java
+++ ComandosCondicionais/src/ComandoCondicional.java
@@ -4,18 +4,20 @@ public class ComandoCondicional {
 	public static void main(String[] args) {
 		// IF
 		
-		int num1 = 10, num2 = 30;
+		int num1 = 10;
+		int num2 = 30;
+
 		if (num1 < num2) {
 			System.out.println("if normal - Número 1 é menor que número 2");
 		}
-		
+
 		if (num1 > num2) {
-			System.out.println("if com else - Número 1 é maior que número 2");			
+			System.out.println("if com else - Número 1 é maior que número 2");
 		} else {
 			System.out.println("if com else - Número 1 é menor que número 2");
 		}
 		
-		// condi��o composta - situa��o num1 < num2
+		// condição composta - situação num1 < num2
 		if (num1 > num2) {
 			System.out.println("Condição composta 1 - Número 1 é maior que número 2");	
 		} else if (num1 < num2) {
